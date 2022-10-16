ed6a079576487e6c46a07f504fd3cd82916a6bb7
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-10-16 20:03:42 +0200

    appear on load added for navbar & upper section

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const upperSection = document.querySelector('.upper-section')
+const navBar = document.querySelector('.navbar')
+
+function myFunction() {
+    upperSection.style.opacity = "1";
+    upperSection.style.transition = "1.5s";
+    navBar.style.opacity = "1";
+    navBar.style.transition = "1.5s"
+
+    
+}
+
+myFunction();
+
+
+
 const slideIn = document.querySelector('.slide-in');
 
 window.addEventListener('scroll', () => {
