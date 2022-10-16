ee078ffa61115f9b9d2bfae4cc375628b1f6c77d
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-10-16 20:44:56 +0200

    JS changes, new effect for mid section

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-const upperSection = document.querySelector('.upper-section')
-const navBar = document.querySelector('.navbar')
+// SHOW ON LOAD FUNCTION
+
+const upperSection = document.querySelector(".upper-section");
+const navBar = document.querySelector(".navbar");
 
 function myFunction() {
-    upperSection.style.opacity = "1";
-    upperSection.style.transition = "1.5s";
-    navBar.style.opacity = "1";
-    navBar.style.transition = "1.5s"
-
-    
+  upperSection.style.opacity = "1";
+  upperSection.style.transition = "1.5s";
+  navBar.style.opacity = "1";
+  navBar.style.transition = "1.5s";
 }
 
 myFunction();
 
+// SLIDE IN FUNCTION
 
+const slideIn = document.querySelector(".slide-in");
 
-const slideIn = document.querySelector('.slide-in');
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight } = document.documentElement;
 
-window.addEventListener('scroll', () => {
-    const {scrollTop, clientHeight} = document.documentElement;
+  //console.log(scrollTop, clientHeight);
 
-   //console.log(scrollTop, clientHeight);
+  const topElementToTopViewport = slideIn.getBoundingClientRect().top;
 
-   const topElementToTopViewport = slideIn.getBoundingClientRect().top;
-   
-    if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight *1.2) {
-        slideIn.classList.add('active')
-    }
-})
+  if (
+    scrollTop >
+    (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 1.2
+  ) {
+    slideIn.classList.add("active");
+  }
+});
+
+// FUNCTION FOR PROJECTS, COME IN LEFT AND RIGHT
